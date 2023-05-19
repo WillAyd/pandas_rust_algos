db2d2a303e51bab48d9a93458a8e320688a2b09d
Will Ayd <william.ayd@icloud.com>
2023-05-19 14:31:08 -0700

    take2d checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use numpy::ndarray::{ArrayView1, ArrayViewMut1};
-use numpy::{PyArray1, PyReadonlyArray1};
+use numpy::ndarray::{ArrayView1, ArrayView2, ArrayViewMut1, ArrayViewMut2, Axis};
+use numpy::{PyArray1, PyReadonlyArray1, PyReadonlyArray2, PyReadwriteArray2};
 use pyo3::prelude::*;
 use pyo3::PyResult;
 use std::fmt::Debug;
@@ -144,5 +144,28 @@ fn pandas_rust_algos(_py: Python, m: &PyModule) -> PyResult<()> {
         }
     }
 
+    fn take_2d(values: ArrayView2<i64>, indexer: ArrayView1<i64>, mut out: ArrayViewMut2<i64>) {
+        let nrows = values.raw_dim()[0];
+        let ncols = indexer.raw_dim()[0];
+
+        for i in 0..nrows {
+            for j in 0..ncols {
+                unsafe {
+                    let idx = *indexer.uget(j);
+                    *out.uget_mut((i, j)) = *values.uget((i, idx as usize));
+                }
+            }
+        }
+    }
+
+    #[pyfn(m)]
+    #[pyo3(name = "take_2d")]
+    fn take_2d_py<'py>(
+        values: PyReadonlyArray2<i64>,
+        indexer: PyReadonlyArray1<i64>,
+        mut out: PyReadwriteArray2<i64>,
+    ) {
+        take_2d(values.as_array(), indexer.as_array(), out.as_array_mut())
+    }
     Ok(())
 }
