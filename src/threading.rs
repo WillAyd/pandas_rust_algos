03d2c04183fceaf2d1292657530e1607cb68c031
Will Ayd <william.ayd@icloud.com>
2023-06-06 20:12:44 -0700

    threading updates

--- src/threading.rs
+++ src/threading.rs
@@ -1,5 +1,37 @@
-use numpy::ndarray::ArrayViewMut2;
+use numpy::ndarray::{ArrayViewMut1, ArrayViewMut2};
 use std::cell::UnsafeCell;
+
+#[derive(Copy, Clone)]
+pub struct UnsafeArrayView1<'a, T> {
+    pub array: &'a UnsafeCell<ArrayViewMut1<'a, T>>,
+}
+
+unsafe impl<'a, T: Copy> Send for UnsafeArrayView1<'a, T> {}
+unsafe impl<'a, T: Copy> Sync for UnsafeArrayView1<'a, T> {}
+
+impl<'a, T> UnsafeArrayView1<'a, T> {
+    pub fn new(array: &'a mut ArrayViewMut1<T>) -> Self {
+        let ptr = array as *mut ArrayViewMut1<T> as *const UnsafeCell<ArrayViewMut1<T>>;
+        Self {
+            array: unsafe { &*ptr },
+        }
+    }
+
+    pub unsafe fn read(&self, i: usize) -> T
+    where
+        T: Copy,
+    {
+        let ptr = self.array.get();
+        *(*ptr).uget(i)
+    }
+
+    /// SAFETY: It is UB if two threads write to the same index without
+    /// synchronization.
+    pub unsafe fn write(&self, i: usize, value: T) {
+        let ptr = self.array.get();
+        *(*ptr).uget_mut(i) = value;
+    }
+}
 
 #[derive(Copy, Clone)]
 pub struct UnsafeArrayView2<'a, T> {
@@ -15,6 +47,14 @@ impl<'a, T> UnsafeArrayView2<'a, T> {
         Self {
             array: unsafe { &*ptr },
         }
+    }
+
+    pub unsafe fn read(&self, i: usize, j: usize) -> T
+    where
+        T: Copy,
+    {
+        let ptr = self.array.get();
+        *(*ptr).uget((i, j))
     }
 
     /// SAFETY: It is UB if two threads write to the same index without
